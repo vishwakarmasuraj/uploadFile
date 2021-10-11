532e399a3303b9a41aa448043aa0426a2fb45892
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2021-10-11 13:38:50 +0530

    1.38 pm
    some chnages

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(routes)
 
 mongoose
   .connect(
